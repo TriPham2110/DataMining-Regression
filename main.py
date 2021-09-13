c5561a978276cbbe98435d2930a635325816bbf0
TriPham2110 <thientri399@gmail.com>
2021-09-13 13:13:30 -0500

    Added method to handle univariate regression for individual feature

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from pandas import read_excel
 from regression import UnivariateRegression
 
+
+def UnivariateFeature(X, y, col, max_iteration=100):
+    if col < 0 or col >= X.shape[1]:
+        return "Incorrect column index!"
+    univariate_feature = X.iloc[:, col]
+    model = UnivariateRegression()
+    model.train(univariate_feature, y, max_iteration=max_iteration)
+    model.plot()
+
+
 if __name__ == '__main__':
     dataset = read_excel('data/Concrete_Data.xls')
     dataset.dropna(axis="columns", how="any", inplace=True)
@@ -8,12 +18,10 @@ if __name__ == '__main__':
     X = dataset.iloc[:, 0:-1]
     y = dataset.iloc[:, -1]
 
-    cement = X.iloc[:, 0]
-
-    model_1 = UnivariateRegression()
-
-    model_1.train(cement, y, max_iteration=100)
-    model_1.plot()
+    '''
+    Univariate regression for individual feature of Concrete Data
+    '''
+    UnivariateFeature(X, y, col=0, max_iteration=100)
 
     '''
     For comparison with scikit-learn
